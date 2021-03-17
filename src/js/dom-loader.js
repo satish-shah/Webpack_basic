b8bb609ee1c3b5fb8ee1c53ad5104d1f556f607b
satish-shah <fireside.satish2012@gmail.com>
2021-03-17 21:51:25 +1100

    webpack pack is added

--- src/js/dom-loader.js
+++ src/js/dom-loader.js
@@ -1,2 +1,4 @@
+// this is exporting the queryselector
+
 export var secretButton = document.querySelector("#secret-button");
 export var secretParagraph = document.querySelector("#secret-paragraph");
